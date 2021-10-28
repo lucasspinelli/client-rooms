ac2e2ef18d82d6e435285d0f8d9e35647958cfc0
Lucas Levi <lucas.lsl12@gmail.com>
2021-10-28 13:25:12 -0300

    add room list html

---
+++ src/app/room.ts
@@ -0,0 +1,8 @@
+export class Room{
+    id:number | undefined;
+    name: string | undefined;
+    date:string | undefined;
+    startHour: string | undefined;
+    endHour:string | undefined;
+    active: boolean | undefined;
+}
