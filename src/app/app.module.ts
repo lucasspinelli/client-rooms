166d23caa189aa3b67373e1da42f584c2cc989ac
Lucas Levi <lucas.lsl12@gmail.com>
2021-10-28 15:13:41 -0300

    change app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,28 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateRoomComponent } from './create-room/create-room.component';
 import { RoomDetailsComponent } from './room-details/room-details.component';
 import { RoomListComponent } from './room-list/room-list.component';
+import { HttpClientModule } from '@angular/common/http';
 import { UpdateRoomComponent } from './update-room/update-room.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     CreateRoomComponent,
+    UpdateRoomComponent,
     RoomDetailsComponent,
     RoomListComponent,
-    UpdateRoomComponent
+    RoomListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
