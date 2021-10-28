245a13caa6da23ef13b4e5b306e9c21f82fff998
Lucas Levi <lucas.lsl12@gmail.com>
2021-10-28 14:07:53 -0300

    initialize variabkes

--- src/app/room.ts
+++ src/app/room.ts
@@ -1,8 +1,8 @@
 export class Room{
-    id:number | undefined;
-    name: string | undefined;
-    date:string | undefined;
-    startHour: string | undefined;
-    endHour:string | undefined;
-    active: boolean | undefined;
+    id:number = 0;
+    name: string = "";
+    date:string = "";
+    startHour: string = "";
+    endHour:string = "";
+    active: boolean = false;
 }
