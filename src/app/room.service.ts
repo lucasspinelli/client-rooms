f0cb196d69ed57cc8b19ae5aefe8be193dde4477
Lucas Levi <lucas.lsl12@gmail.com>
2021-10-28 13:49:26 -0300

    methods for rooms service

--- src/app/room.service.ts
+++ src/app/room.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoomService {
 
-  constructor() { }
+
+  constructor(private http: HttpClient) { }
+
+  private baseUrl = 'http://localhost:8080/api/v1/rooms';
+
+  getRoom(id: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}/${id}`);
+  }
+
+  createRoom(room: Object): Observable<Object> {
+    return this.http.post(`${this.baseUrl}`, room);
+  }
+
+  updateRoom(id: number, value: any): Observable<Object> {
+    return this.http.put(`${this.baseUrl}/${id}`, value);
+  }
+
+  deleteRoom(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/${id}`, { responseType: 'text' });
+  }
+
+  getRoomsList(): Observable<any> {
+    return this.http.get(`${this.baseUrl}`);
+  }
 }
